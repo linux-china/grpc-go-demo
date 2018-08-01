9264811f00eb8d5e78f39ddeb9b941ea2c843d04
linux_china <linux_china@hotmail.com>
2018-07-31 20:44:26 -0700

    go grpc server

---
+++ main.go
@@ -0,0 +1,39 @@
+//go:generate protoc -I ./greeter_api --go_out=plugins=grpc:./greeter_api ./greeter_api/greeter.proto
+
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/linux_china/grpc-demo/greeter_api"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port = ":50051"
+)
+
+// server is used to implement greeter.GreeterServer.
+type server struct{}
+
+// SayHello implements greeter.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	return &pb.HelloReply{Message: "Hello " + in.Name + "!"}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
